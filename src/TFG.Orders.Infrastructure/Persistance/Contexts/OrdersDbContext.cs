ad331605b1709e0ce35a5b1d03599682db6a95f4
Adrián Ortega Bellido <aortega@plainconcepts.com>
2022-03-29 20:19:41 +0200

    Added Database

--- src/TFG.Orders.Infrastructure/Persistance/Contexts/OrdersDbContext.cs
+++ src/TFG.Orders.Infrastructure/Persistance/Contexts/OrdersDbContext.cs
@@ -6,5 +6,15 @@ namespace TFG.Orders.Infrastructure.Persistance.Contexts
     public class OrdersDbContext : DbContext
     {
         public DbSet<Order> Orders { get; set; }
+
+        public OrdersDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(OrdersDbContext).Assembly);
+        }
+
     }
 }
